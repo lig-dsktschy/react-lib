b648cb748d5fb4a1464f7c7b91e270cdcd9933ac
daisuke-tsuchiya-lig <tsuchiya_daisuke@liginc.co.jp>
2021-01-08 12:44:59 +0900

    Bundle language files

--- src/scripts/button1.js
+++ src/scripts/button1.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 import $$ from '../stylesheets/button1.scss'
 
-const Button = () => (
+import __ from 'i18next';
+import en from '../languages/button1/en.json'
+import jp from '../languages/button1/jp.json'
+import ko from '../languages/button1/ko.json'
+import sc from '../languages/button1/sc.json'
+import tc from '../languages/button1/tc.json'
+
+const defaultLng = 'en'
+__.init({
+  lng: defaultLng,
+  keySeparator: '|',
+  resources: { en, jp, sc, tc, ko }
+}, (error) => {
+  if (error) console.error(error)
+})
+
+const Button = props => (
   <button class={$$['button']}>
-    Submit
+    {__.t('button:Submit', { lng: props.lng || defaultLng })}
   </button>
 )
 
